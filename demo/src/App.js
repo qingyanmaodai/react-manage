3a03e829d9cbc8a9fd9b7ba8eba4453fad8ddfe1
qingyanmaodai <923340767@qq.com>
2023-02-15 03:46:40 +0800

    创建项目并修改

---
+++ demo/src/App.js
@@ -0,0 +1,13 @@
+// 导入App组件的样式
+import './App.css';
+// 采用函数方式创建App组件
+function App() {
+  // 函数返回的是组件的内容
+  return (
+    <div className="App">
+      App
+    </div>
+  );
+}
+
+export default App;
